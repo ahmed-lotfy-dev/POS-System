85b3159b220d7ce7c11f569553748109a620b114
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-06-15 23:19:35 +0300

    add dockerfiles for client and server ,docker-compose.yml

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+require('dotenv').config()
+const cors = require("cors")
+
+server.use(cors())
+server.use(middlewares)
+
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+  console.log(process.env.TESTING);
+
+})
