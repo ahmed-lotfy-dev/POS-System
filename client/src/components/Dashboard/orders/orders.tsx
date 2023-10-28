b74d6d4645d18b47cd1a38b20a4db99f01233333
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-10-29 01:55:06 +0200

    work on orders page

--- client/src/components/Dashboard/orders/orders.tsx
+++ client/src/components/Dashboard/orders/orders.tsx
@@ -1,5 +1,15 @@
 import { Card } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { AllDataResponse } from "@/types/globals";
+import { useState } from "react";
 import { useRouteLoaderData } from "react-router-dom";
 
 type Props = {};
@@ -9,27 +19,63 @@ const Orders = ({}: Props) => {
   console.log(orders);
 
   // Get the current date and format it as "MM/DD/YYYY"
-  const todayDate = new Date().toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
+  const todayDate = new Date()
+    .toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .split("/")[0];
 
   console.log("todayDate:", todayDate);
 
   // Filter orders with orderDate equal to the current date
   const filteredOrders = orders.filter((order) => {
-    const orderDate = new Date(order.orderDate).toLocaleDateString("en-US", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-    });
+    const orderDate = new Date(order.orderDate)
+      .toLocaleDateString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+      })
+      .split("/")[0];
     console.log("Order Date:", orderDate);
     return orderDate === todayDate;
   });
 
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   return (
     <div className="flex flex-col justify-center items-center w-full">
+      <div>
+        <Select >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a month" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Months</SelectLabel>
+              {months.map((month, index) => (
+                <SelectItem key={index} value={month}>
+                  {month}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
       <h2>normal all orders</h2>
       <div className="flex flex-wrap">
         {orders.map((order) => (
@@ -41,7 +87,7 @@ const Orders = ({}: Props) => {
         ))}
       </div>
       <h2>filteredOrders</h2>
-      <div>
+      <div className="flex flex-wrap">
         {filteredOrders.map((order) => (
           <Card className="m-5" key={order.orderNumber}>
             <h2>{order.userId}</h2>
